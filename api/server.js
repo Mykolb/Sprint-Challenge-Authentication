0382ef53afac3018645fac56a73ecc05ff6647d8
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-05-17 11:44:10 -0400

    server running, working on routes

--- api/server.js
+++ api/server.js
@@ -12,4 +12,8 @@ server.use(express.json());
 
 configureRoutes(server);
 
+server.get('/', (req, res) => {
+    res.send(`<h2>We have data showing!</h2>`)
+});
+
 module.exports = server;
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,11 @@
 const axios = require('axios');
-
+const db = require('./routes-model');
 const { authenticate } = require('../auth/authenticate');
+//add import for hash 
+const bcrypt = require('bcryptjs');
+//import json token and secrets file 
+const jwt = require('jsonwebtoken');
+
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -8,13 +13,48 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+//POST for user registration 
 function register(req, res) {
   // implement user registration
+  let user = req.body; //user contains plain txt pwd/username
+  const hash = bcrypt.hashSync(user.password, 5)
+  //overrride user.pwd with hashed pwd 
+  user.password = hash;
+
+  db.add(user)
+  .then(saved => {
+      res.status(201).json(saved)
+  })
+  .catch(err => {
+      res.status(500).json({error: err.message})
+  })
 }
 
+
+
+
+//POST for user login
 function login(req, res) {
   // implement user login
-}
+  let { username, password } = req.body;
+
+  db.findBy({ username })
+     .first()
+     .then(user => {
+         //if pwds match....
+         if(user && bcrypt.compareSync(password, user.password)) {
+             const token = generateToken(user); //add token
+             res.status(201).json({ message: `You are logged in as ${user.username}!, please accept this token`, token }); //pass in token 
+         } else {
+             res.status(401).json({message: 'You shall not pass!' })
+         }
+     })
+     .catch(err => {
+         res.status(500).json({ error: err.message})
+     })
+ }
+
+
 
 function getJokes(req, res) {
   const requestOptions = {
@@ -30,3 +70,19 @@ function getJokes(req, res) {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
 }
+
+
+const generateToken = user => {
+  const payload = {
+      subject: user.id, //what token is about 
+      username: user.username,
+      //other data 
+      // department: [''], add later 
+  }
+  const options = {
+      expiresIn: '72h'
+  }
+  return jwt.sign(payload, jwtKey, options); 
+}
+
+
